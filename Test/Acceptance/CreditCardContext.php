b5af63dd859bb93f8c66d24e1c315841c7f991ad
Luiz Felipe (lflimeira) <lflimeira02@gmail.com>
2018-03-26 18:01:59 -0300

    tests: refactor CC tests
    Implement an abstract class for payment contexts
    that allows code reutilization.

--- Test/Acceptance/CreditCardContext.php
+++ Test/Acceptance/CreditCardContext.php
@@ -1,23 +1,9 @@
 <?php
 
-use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use Behat\MinkExtension\Context\RawMinkContext;
 use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 
-/**
- * Defines application features from the specific context.
- */
-class CreditCardContext extends RawMinkContext
+class CreditCardContext extends PaymentContext
 {
-    use CustomerConfigProvider;
-    use ModuleConfigProvider;
-    use InstallmentsConfigProvider;
-    use SessionWait;
-
-    private $customer;
-    private $page;
 
     /**
      * @beforeSuite
@@ -50,125 +36,34 @@ class CreditCardContext extends RawMinkContext
         }
     }
 
-    /**
-     * @Given a registered user
-     */
     public function aRegisteredUser()
     {
-       $this->customer =  $this->getCustomer(); 
+        parent::aRegisteredUser();
     }
 
-    /**
-     * @When I access the store page
-     */
     public function iAccessTheStorePage()
     {
-        $this->getSession()->visit(
-            $this->locatePath('/')
-        );
+        parent::iAccessTheStorePage();
     }
 
-    /**
-     * @When add any product to basket
-     */
     public function addAnyProductToBasket()
     {
-        $this->page = $this->getSession()->getPage();
-        $this->page->pressButton("Add to Cart");
+        parent::addAnyProductToBasket();
     }
 
-    /**
-     * @When I go to checkout page
-     */
     public function iGoToCheckoutPage()
     {
-        $this->getSession()->visit(
-            $this->locatePath('/checkout')
-        );
-        
-        $page = $this->page;
-
-        try {
-            $this->page->pressButton('Sign In');
-        } catch(\Exception $exception) {
-            $this->spin(
-                function($context) use ($page) {
-                    $loader = $page->find('css', '.checkout-loader');
-                    if(is_null($loader)) {
-                        return true;
-                    }
-
-                    return false;
-                }
-            , 120);
-            $this->page->pressButton('Sign In');
-        } 
+        parent::iGoToCheckoutPage();
     }
 
-    /**
-     * @When login with registered user
-     */
     public function loginWithRegisteredUser()
     {
-        $page = $this->page;
-        
-        $this->spin(
-            function($context) use($page) {
-                return (
-                    $page->find('css', '.authentication-dropdown')->isVisible() &&
-                    $page->find('css', '#login-email')->isVisible() &&
-                    $page->find('css', '#login-password')
-                );
-            }
-        );
-        
-        $this->page->fillField(
-            'username',
-            $this->customer->username
-        );
-
-        $this->page->fillField(
-            'password',
-            $this->customer->password
-        );
-
-        $this->page->find('css', '.action-login.secondary')->click();
-
-        $this->spin(
-            function($context) use($page) {
-                return ($page->find('css', '.shipping-address-item'));
-            }
-        );
-
-        $shippingAddres = $page->find('css', '.shipping-address-item')->getText();
-
-        if(strpos($shippingAddres, $this->customer->name) === false){
-            throw new Exception();
-        }
-
-        $this->getSession()->wait(2000);
-        $this->page->pressButton('Next');
+        parent::loginWithRegisteredUser();
     }
 
-    /**
-     * @When choose pay with transparent checkout using credit card
-     */
-    public function choosePayWithTransparentCheckoutUsingCreditCard()
+    public function choosePayWithTransparentCheckout($paymentMethod)
     {
-        $page = $this->page;
-
-        $this->spin(
-            function($context) use($page) {
-                return ($page->find('css', '#mundipagg_creditcard'));
-            }
-        );
-
-        $this->getSession()->wait(2000);
-
-        $this->page->find(
-            'css',
-            '#mundipagg_creditcard'
-        )->selectOption('mundipagg_creditcard');
+        parent::choosePayWithTransparentCheckout($paymentMethod);
     }
 
     /**
@@ -180,77 +75,37 @@ class CreditCardContext extends RawMinkContext
             'css',
             '#mundipagg_creditcard_cc_number'
         )->setValue('4242424242424242');
-
         $this->page->find(
             'css',
             '#mundipagg_creditcard_cc_owner'
         )->setValue($this->customer->name);
-        
+
         $this->page->find(
             'css',
             '#mundipagg_creditcard_expiration'
         )->selectOption('12');
-        
+
         $this->page->find(
             'css',
             '#mundipagg_creditcard_expiration_yr'
         )->selectOption('2028');
-
         $this->page->find(
             'css',
             '#mundipagg_creditcard_cc_cid'
         )->setValue('123');
-
         $this->page->find(
             'css',
             '#mundipagg_creditcard_installments'
         )->selectOption($installment);
     }
 
-    /**
-     * @When place order
-     */
     public function placeOrder()
     {
-        $page = $this->page;
-        $this->spin(
-            function($context) use($page) {
-                return ($page->find(
-                    'css',
-                    '.payment-method._active button.primary.checkout'
-                ));
-            }
-        );
-        $this->page->find(
-            'css',
-            '.payment-method._active button.primary.checkout'
-        )->click();
+        parent::placeOrder();
     }
 
-    /**
-     * @Then the purchase must be paid with success
-     */
     public function thePurchaseMustBePaidWithSuccess()
     {
-        $page = $this->page;
-        $this->spin(
-            function($context) use ($page) {
-                $pageTitle = $this->page->find('css', '.page-title-wrapper')->getText();
-                if(strpos($pageTitle, 'Thank you for your purchase!') === false) {
-                    return false;
-                }
-                return true;
-            }
-        );
-    }
-
-    public function getCustomer()
-    {
-        $customer = new stdClass;
-        $customer->name = 'Alan Turing';
-        $customer->username = 'alan@turing.com';
-        $customer->password = '##Abc123456##';
-
-        return $customer;
+        parent::thePurchaseMustBePaidWithSuccess();
     }
 }
