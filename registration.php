0fe8c6c8ec66d8860b32c71922132c229b78cd3f
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-03-31 16:11:20 -0300

    config: change module's name

--- registration.php
+++ registration.php
@@ -8,6 +8,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MundiPagg_MundiPagg',
+    'PagarMe_Magento2',
     __DIR__
 );
